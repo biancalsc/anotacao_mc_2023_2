5e417adaeff963bc1b8317bf7ee67b23c0d3def9
Bianca Lucas <biancalsc1409@gmail.com>
2023-08-25 22:36:46 -0300

    matematica

---
+++ mathematical_definitions/definitions/iterative_sequence.ts
@@ -0,0 +1,11 @@
+function sequence(n: number): number {
+    let term: number = 1;
+    let limit: number = 2;
+    while (limit <= n) {
+        term = term * 2;
+        limit = limit + 1
+    }
+    return term
+}
+
+console.log(sequence(10));
---
+++ mathematical_definitions/definitions/recursive_sequence.ts
@@ -0,0 +1,11 @@
+function sequence(n: number): number {
+    if (n == 1) {
+        return 1;
+    } else {
+        return 2 * sequence(n - 1);
+    }
+}
+
+console.log(sequence(10));
+
+export{};
